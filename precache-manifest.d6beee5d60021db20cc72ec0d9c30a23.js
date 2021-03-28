59b99dcd3add1e948fb5463e11603ec57523da93
Code Sandbox <hello@codesandbox.io>
2021-03-28 15:37:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d6beee5d60021db20cc72ec0d9c30a23.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ebc90732691bdfeabd01d1ce8a2213",
+    "url": "https://keannachicco.github.io/csb-kqqvf/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "https://keannachicco.github.io/csb-kqqvf/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "4942ed1c43052e1cf469",
+    "url": "https://keannachicco.github.io/csb-kqqvf/static/js/main.b86aa8ea.chunk.js"
+  },
+  {
+    "revision": "993b8dbfbb063b2f7bde",
+    "url": "https://keannachicco.github.io/csb-kqqvf/static/js/runtime-main.184eabfd.js"
+  }
+]);
